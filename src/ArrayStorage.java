7dd84e1cbcdfda55d1fca0bb682b82af89296409
Keryit <kery85@ukr.net>
2018-10-23 23:33:04 +0300

    Implemented methods save,delete,get,getAll,clear, size

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,76 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int count = 0;
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null)
+                break;
+            storage[i] = null;
+            count = 0;
+        }
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        if (storage[count] == null)
+            storage[count] = resume;
+        count++;
+
     }
 
     Resume get(String uuid) {
+        for (Resume aStorage : storage) {
+            if (aStorage != null && uuid.equals(aStorage.uuid))
+                return aStorage;
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int deletedIndex = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                count--;
+                deletedIndex = i;
+            }
+        }
+
+        for (int i = deletedIndex; i < storage.length - 1; i++) {
+            if (storage[i + 1] == null)
+                break;
+            Resume tmp = storage[i];
+            storage[i] = storage[i + 1];
+            storage[i + 1] = tmp;
+
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] res = new Resume[size()];
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null)
+                break;
+
+            res[i] = storage[i];
+        }
+        return res;
     }
 
     int size() {
-        return 0;
+
+        int countSize = 0;
+        for (Resume aStorage : storage) {
+            if (aStorage == null)
+                break;
+
+            countSize++;
+        }
+        return countSize;
     }
 }
